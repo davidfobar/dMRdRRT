0cabddac40362ac1796bf54f697dffea25340181
Dave Fobar <david.fobar@gmail.com>
2026-05-04 12:39:07 -0600

    removed excess files

--- utils/visualization.py
+++ utils/visualization.py
@@ -2,10 +2,7 @@ from __future__ import annotations
 
 from pathlib import Path
 from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from Agent import Agent
-
+from agent import Agent
 
 def plot_agent(
     agent: Agent,
